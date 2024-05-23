de405bf3c4759aced6e9220af80cf396eef9c090
신우찬 <8621skyblue@naver.com>
2024-05-23 23:05:26 +0900

    메인파일 위치 지정

--- clone_coding/settings.py
+++ clone_coding/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'clone_coding.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
