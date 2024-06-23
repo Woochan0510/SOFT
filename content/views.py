15502e337090de092aaa4cc12a32fcfdeab040e6
신우찬 <8621skyblue@naver.com>
2024-06-23 19:18:43 +0900

    db 생성 및 기초 작업

--- content/views.py
+++ content/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
 # Create your views here.
+
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all()
+
+        return render(request, "silvergram/main.html", context=dict(feeds=feed_list))
