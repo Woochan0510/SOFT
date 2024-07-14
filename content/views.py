b168505393701a6b671711cfc29950d498ae460f
신우찬 <8621skyblue@naver.com>
2024-07-14 15:02:46 +0900

    파일 업로드 API 구현 완료

--- content/views.py
+++ content/views.py
@@ -1,7 +1,11 @@
+from uuid import uuid4
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed
+import os
+from clone_coding.settings import MEDIA_ROOT
 
 # Create your views here.
 
@@ -13,11 +17,22 @@ class Main(APIView):
     
 class UploadFeed(APIView):
     def post(self, request):
+        
+        #일단 파일 불러오기
+        file = request.FILES['file']
+        uuid_name = uuid4().hex #랜덤하게 이름을 만들어줌
+        save_path = os.path.join(MEDIA_ROOT, uuid_name) #위에서 생성한 랜덤한 이름으로 루트 지정
+        
+        with open(save_path, 'wb+') as destination: #실제 파일을 저장하는 부분
+            for chunk in file.chunks():
+                destination.write(chunk)
 
         file = request.data.get('file')
-        image = request.data.get('image')
+        image = uuid_name
         content = request.data.get('content')
         user_id = request.data.get('user_id')
         profile_image = request.data.get('profile_image')
+
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image,like_count=0)
 
         return Response(status=200)
