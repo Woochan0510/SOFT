1dfba1c3b240265b835436f76fdec83a44197b57
신우찬 <8621skyblue@naver.com>
2024-07-14 14:39:11 +0900

    피드 업로드 API 구현중

--- content/views.py
+++ content/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed
 
@@ -9,3 +10,14 @@ class Main(APIView):
         feed_list = Feed.objects.all().order_by('-id') # select * from content_feed
 
         return render(request, "silvergram/main.html", context=dict(feeds=feed_list))
+    
+class UploadFeed(APIView):
+    def post(self, request):
+
+        file = request.data.get('file')
+        image = request.data.get('image')
+        content = request.data.get('content')
+        user_id = request.data.get('user_id')
+        profile_image = request.data.get('profile_image')
+
+        return Response(status=200)
