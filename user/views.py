4a8e26990a412ec8ec87edaf3242920a6ac088b1
신우찬 <8621skyblue@naver.com>
2024-07-15 10:59:07 +0900

    사이트 주소 추가

--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
 # Create your views here.
+class Register(APIView):
+    def get(self, request):
+        return render(request, "user/register.html")
