54b271cd1cf615d7a0a90cc5ea9f2cf70477f603
신우찬 <8621skyblue@naver.com>
2024-07-16 20:41:01 +0900

    로그인 페이지 추가

--- clone_coding/urls.py
+++ clone_coding/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from content.views import Main, UploadFeed
-from user.views import Register
+from user.views import Register, Login
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,7 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', Main.as_view()),
     path('content/upload', UploadFeed.as_view()),
-    path('user/register', Register.as_view())
+    path('user/register', Register.as_view()),
+    path('user/login', Login.as_view())
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -5,3 +5,15 @@ from rest_framework.views import APIView
 class Register(APIView):
     def get(self, request):
         return render(request, "user/register.html")
+    
+    def post(self, request):
+        #TODO: 회원가입
+        pass
+    
+class Login(APIView):
+    def get(self, request):
+        return render(request, "user/login.html")
+    
+    def post(self, request):
+        #TODO: 로그인
+        pass
